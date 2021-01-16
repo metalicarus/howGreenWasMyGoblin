b591801abff234a7ffce2c982440b212cfbfd5ad
metalicarus <silva.matheusms.mateus83@gmail.com>
2021-01-16 11:11:44 -0300

    tomcat settings

--- src/main/java/com/hobgoblin/howGreenWasMyGoblin/HowGreenWasMyGoblinApplication.java
+++ src/main/java/com/hobgoblin/howGreenWasMyGoblin/HowGreenWasMyGoblinApplication.java
@@ -2,13 +2,14 @@ package com.hobgoblin.howGreenWasMyGoblin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
-public class HowGreenWasMyGoblinApplication {
+public class HowGreenWasMyGoblinApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		
